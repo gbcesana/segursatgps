6509878aaf972d99ba5558135fe403aefa59d088
jpalaciosg <jpalacios@segursat.com>
2023-02-09 04:38:26 +0000

    cambiando el consumer a asincrono

--- segursatgps/asgi.py
+++ segursatgps/asgi.py
@@ -1,16 +1,29 @@
-#import os
+import os
 import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'segursatgps.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'segursatgps.settings.production')
 django.setup()
 
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 import segursatgps.routing
 
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                segursatgps.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
+
+"""
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
@@ -19,3 +32,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+"""
--- segursatgps/consumers.py
+++ segursatgps/consumers.py
@@ -1,7 +1,52 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+#from asgiref.sync import async_to_sync
+#from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
+class GenericConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        #message = text_data_json['message']
+        message = text_data_json
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    async def send_message(self, event):
+        message = event['message']
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+"""
 class GenericConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -44,3 +89,4 @@ class GenericConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': message
         }))
+"""
