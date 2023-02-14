10759f345268e8ccdb5286c21381e11293b36291
jpalaciosg <jpalacios@segursat.com>
2023-02-14 04:17:07 +0000

    regresando configuracion para desarrollo

--- segursatgps/wsgi.py
+++ segursatgps/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'segursatgps.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'segursatgps.settings.development')
 application = get_wsgi_application()
