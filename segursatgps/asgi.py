413e51d7a4b13f6bfe74ab6e398403592464f9e1
jpalaciosg <jpalacios@segursat.com>
2023-02-15 14:38:59 +0000

    configuracion para pase a produccion de la release 0.99.6

--- segursatgps/asgi.py
+++ segursatgps/asgi.py
@@ -4,7 +4,7 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'segursatgps.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'segursatgps.settings.production')
 django.setup()
 
 from channels.auth import AuthMiddlewareStack
